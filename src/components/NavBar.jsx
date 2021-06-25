29dcf5ebbb9275d55209bf81b33d71b397a38d66
Nature711 <e0665488@u.nus.edu>
2021-06-25 11:35:20 +0800

    update navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,87 @@
 import React from "react";
 import logo from "../images/notepad.png";
 import { LightButton } from "../globalStyles";
-import "./NavBar.css";
+import styled from "styled-components/macro";
+
+const NavContainer = styled.nav`
+  background: linear-gradient(
+    90deg,
+    rgba(88, 81, 208, 1) 0%,
+    rgba(217, 40, 186, 1) 35%,
+    rgba(44, 205, 237, 1) 100%
+  );
+  height: 80px;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavIcon = styled.img`
+  padding-left: 10px;
+  width: 55px;
+  height: 55px;
+`;
+
+const Title = styled.p`
+  color: #fff;
+  margin-left: 10px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.8rem;
+  font-weight: 1000;
+  font-family: "papyrus";
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  height: 100%;
+`;
+
+const MenuLink = styled.a`
+  color: #fff;
+  padding: 0.5rem 1rem;
+  font-size: x-large;
+  &:hover {
+    border-bottom: 3px solid #fff;
+    transition: all 0.2s linear;
+    color: #663399;
+  }
+`;
 
 function NavBar() {
   return (
-    <nav>
-      <div class="LogoContainer">
-        <img id="NavIcon" src={logo} alt="logo" />
-        <a id="Title" href="/">
-          MY NUSMODS
-        </a>
-      </div>
-      <div class="Menu">
-        <a class="MenuLink" to="/">
+    <NavContainer>
+      <LogoContainer>
+        <NavIcon src={logo} alt="logo" />
+        <Title href="/">MY NUSMODS</Title>
+      </LogoContainer>
+      <Menu>
+        <MenuLink class="MenuLink" to="/">
           Link1
-        </a>
-        <a class="MenuLink" to="/">
+        </MenuLink>
+        <MenuLink class="MenuLink" to="/">
           Link2
-        </a>
-        <a class="MenuLink" to="/">
+        </MenuLink>
+        <MenuLink class="MenuLink" to="/">
           Link3
-        </a>
-      </div>
+        </MenuLink>
+      </Menu>
       <div style={{ margin: "10px" }}>
         <LightButton>LOG IN</LightButton>
       </div>
-    </nav>
+    </NavContainer>
   );
 }
 
